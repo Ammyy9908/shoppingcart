f999b7217d64c86b59eeaf1e324992415fe36052
Ammyy9908 <sb78639@gmail.com>
2020-04-02 20:41:44 +0530

    Updates

---
+++ precache-manifest.3dce41de1f7ec372557d509bc2a66ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0fd402a77aa28cbbbeb6d5248e8bd5",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "1c7161fe421b6d5051fc",
+    "url": "/shoppingcart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65fdb28f4a7bdbd678a0",
+    "url": "/shoppingcart/static/js/2.8df037f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingcart/static/js/2.8df037f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7161fe421b6d5051fc",
+    "url": "/shoppingcart/static/js/main.1382492a.chunk.js"
+  },
+  {
+    "revision": "1f88b3cd8a330df6dd8f",
+    "url": "/shoppingcart/static/js/runtime-main.d57733ac.js"
+  }
+]);
